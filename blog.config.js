3bd27cb84ba8ba734a8eebba421133523378d483
LoveMyself546 <37284782+LoveMyself546@users.noreply.github.com>
2022-06-17 00:22:00 +0800

    更新

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   AUTHOR: 'Lee546', // 作者
   BIO: '一条有梦想的咸鱼', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
-  AVATAR: '/myself.png', // 个人头像 默认取public目录下的avatar.png
+  AVATAR: '/avatar.png', // 个人头像 默认取public目录下的avatar.png
   KEYWORDS: 'web, Notion, 博客，技术', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
